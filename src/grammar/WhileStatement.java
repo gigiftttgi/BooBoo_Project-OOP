94ca8b3379e540aaa0e3beb28f62089a8b9e32af
gigiftttgi <natcha_si@cmu.ac.th>
2022-02-14 17:40:11 +0700

    whilestat

--- src/grammar/WhileStatement.java
+++ src/grammar/WhileStatement.java
@@ -1,5 +1,27 @@
 package grammar;
 
-public class WhileStatement {
-    
+public class WhileStatement extends StatementNode {
+
+    protected Node expression;
+    protected StatementNode loopStatement;
+
+    WhileStatement(Node expression, StatementNode loopStatement){
+        this.expression = expression;
+        this.loopStatement = loopStatement;
+    }
+
+    public StatementNode getLoopstatem(){
+        return loopStatement;
+    }
+
+    public Node setCondition(Node exp){
+        if(expression == exp){
+            return expression;
+        }
+        else{
+            expression = exp;
+            return expression;
+        }
+    }
+       
 }
