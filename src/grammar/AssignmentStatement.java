6e274294b96dc2dda372cbf01c92fb0a26166b14
gigiftttgi <natcha_si@cmu.ac.th>
2022-02-14 16:14:05 +0700

    assignment

--- src/grammar/AssignmentStatement.java
+++ src/grammar/AssignmentStatement.java
@@ -4,14 +4,14 @@ public class AssignmentStatement implements Node {
     protected Variable variable;
     protected Node expression;
 
-    AssignmentStatement(Node expression){
+    AssignmentStatement(Node expression, Variable variable){
         this.expression = expression;
         this.variable = variable;
     }
 
     @Override
     public double evaluate() {
-        
+        variable.addValue(expression.evaluate());
         return 0;
     }
     
