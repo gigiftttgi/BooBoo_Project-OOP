6e21ffedede3441eb6d8f66829330a3578b67ec8
kkob999 <chom.kappa@gmail.com>
2022-02-16 18:46:15 +0700

    edit random virus

--- src/Model/Characterfactory.java
+++ src/Model/Characterfactory.java
@@ -1,50 +1,46 @@
 package Model;
 public class Characterfactory {
 
-    int currentTime;
-    int virusRate;
+    protected int currentTime;
+    protected int virusRate;
 
-    int increaseVirusrate(int time){
+    protected int increaseVirusrate(int time){
         return virusRate;
     }
 
-    Virus createVirus(int time,int virusRate){
+    public Virus createVirus(int time,int virusRate){
 
-        Virus virus = new Virus();
-        if(time%3==0){
-            Virus_X x = new Virus_X();
+        int randVirus = (int)(Math.random()*3)+1;
+
+        if (randVirus==1) {
             System.out.println("Virus x is now create.");
-            virus = x;
-        }else if(time%5==0){
-            Virus_Y y = new Virus_Y();
+            return new Virus_X();
+        }else if(randVirus==2){
             System.out.println("Virus y is now create.");
-            virus = y;
-        }else if(time%7==0){
-            Virus_Z z = new Virus_Z();
+            return new Virus_Y();
+        }else{
             System.out.println("Virus z is now create.");
-            virus = z;
+            return new Virus_Z();
         }
-
-        return virus;
     }
 
-    Antibody createAntibody(int credit){
-        Antibody anb = new Antibody();
-        if (credit >= 20) {
-
-            System.out.println("Virus A is now create");
-            Antibody_A a = new Antibody_A();
-            anb = a;
-        }else if(credit >= 50){
-            System.out.println("Virus B is now create");
-            Antibody_B b = new Antibody_B();
-            anb = b;
-        }else if(credit >= 70){
-            System.out.println("Virus C is now create");
-            Antibody_C c = new Antibody_C();
-            anb = c;
-        }
-        return anb;
-    }
+    //wait shop to finish
+    // public Antibody createAntibody(int credit){
+    //     Antibody anb = new Antibody();
+    //     if (credit >= 20) {
+    //         System.out.println("Virus A is now create");
+    //         Antibody_A a = new Antibody_A();
+    //         anb = a;
+    //     }else if(credit >= 50){
+    //         System.out.println("Virus B is now create");
+    //         Antibody_B b = new Antibody_B();
+    //         anb = b;
+    //     }else if(credit >= 70){
+    //         System.out.println("Virus C is now create");
+    //         Antibody_C c = new Antibody_C();
+    //         anb = c;
+    //     }
+    //     return anb;
+    // }
     
 }
