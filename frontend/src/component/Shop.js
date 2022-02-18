a257207f50c2c7dbd246c9bcd9a833502bdb1b95
minmeeen <sirapat.mp@gmail.com>
2022-02-19 01:07:33 +0700

    edit shop
    add credit bar and card but they don't show on app :(

--- frontend/src/component/Shop.js
+++ frontend/src/component/Shop.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import ShopCard from './ShopCard';
 
 class Shop extends React.Component {
     render() {
       return (
         <div className="Shop">
           I am Shop
+          <div className="CreditBar">
+            <p className="CreditText">Credit + Money</p>
+          </div>
+
+          <ShopCard>
+            <img src="frontend/src/image/antibody/antiA.png" alt="antiA shop" ></img>
+            
+          </ShopCard>
+          <ShopCard>
+            <img src="frontend/src/image/antibody/antiB.png" alt="antiB shop"></img>
+          </ShopCard>
+          <ShopCard>
+            <img src="frontend/src/image/antibody/antiC.png" alt="antiC shop"></img>
+          </ShopCard>
+
+
         </div>
       );
     }
