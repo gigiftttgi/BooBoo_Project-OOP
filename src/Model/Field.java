691a6c816a4147f727bb7e14dd350f4cce5b5545
kkob999 <chom.kappa@gmail.com>
2022-02-18 22:38:15 +0700

    แก้ createVirus() ไม่ให้เกิดซ้ำ

--- src/Model/Field.java
+++ src/Model/Field.java
@@ -3,31 +3,37 @@ package Model;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Field {
 
+  private List<Virus> listVirus;
+  private List<Antibody> listAntibody;
+  protected int[][] pos = new int[50][50];
 
-    private List<Virus> listVirus;
-    private List<Antibody> listAntibody;
+  public Field() {
+    listVirus = new ArrayList<>();
+    listAntibody = new ArrayList<>();
+  }
 
-    Field(){
-        listVirus = new ArrayList<>();
-        listAntibody = new ArrayList<>();
-    }  
+  public List<Virus> getListVirus() {
+    return listVirus;
+  }
 
-    public List<Virus> getListVirus(){
-        return listVirus;
+  public List<Antibody> getListAntibosy() {
+    return listAntibody;
+  }
+
+  public Virus creatVirus() {
+    Random_int r = new Random_int(51);
+    Characterfactory fac = new Characterfactory();
+    int r1 = r.randomInt();
+    int r2 = r.randomInt();
+    if (pos[r1][r2]==1) {
+      System.out.println("ซ้ำ");
+      return null;
     }
+    pos[r1][r2] = 1;
+    listVirus.add(fac.createVirus(r1, r2));
 
-    public List<Antibody> getListAntibosy(){
-        return listAntibody;
-    }
-
-    public void creatVirus(){
-        Characterfactory fac = new Characterfactory();
-        listVirus.add(fac.createVirus(2, 4));
-        listVirus.add(fac.createVirus(1, 3));
-        listVirus.add(fac.createVirus(3, 3));
-    }
-
+    return null;
+  }
 }
