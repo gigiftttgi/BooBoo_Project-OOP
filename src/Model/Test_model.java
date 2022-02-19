1a60a211a3067f5ed98b0e48c2192e62728810d1
kkob999 <chom.kappa@gmail.com>
2022-02-19 13:53:16 +0700

    test button and shop

--- src/Model/Test_model.java
+++ src/Model/Test_model.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.io.FileNotFoundException;
 
+import Controller.Button;
 import Parser.SyntaxError;
 
 public class Test_model {
@@ -12,4 +13,20 @@ public class Test_model {
     //     a.runGeneticcode();
     //     System.out.println(a.pos.getX_Position() + " " + a.pos.getY_Position());
     // }
+
+    //test Shop.java
+    public static void main(String[] args) {
+        //press the button
+        Button btnA = new Button("Buy Antibody A");
+        Button btnB = new Button("Buy Antibody B");
+        
+       Shop shop = new Shop();
+       System.out.println("Player start money is "+shop.balance);
+       shop.buyAntiA("A", btnA.buyAntiA_btn());
+       shop.buyAntiA("A", btnA.buyAntiA_btn());
+       shop.buyAntiB("B", btnB.buyAntiB_btn());
+       shop.buyAntiB("B", btnB.buyAntiB_btn());
+       System.out.println("Your final balance is "+shop.balance);
+
+    }
 }
