a6dfb24a2a04ab58269c45ae59074cad69169b5b
minmeeen <sirapat.mp@gmail.com>
2022-02-20 01:20:06 +0700

    solve conflict

--- frontend/src/component/Field.js
+++ frontend/src/component/Field.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 const Field = () => {
     return (
         <div className='Field'>
-            
             <td className="w-6 h-6 cursor-pointer">
-                
             </td>
         </div>
     );
