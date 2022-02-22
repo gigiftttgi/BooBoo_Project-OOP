c0811e33fd584afe623b2d9f080da7d60e2fd55d
minmeeen <sirapat.mp@gmail.com>
2022-02-23 04:30:23 +0700

    for control game

--- CAREN/src/main/java/com/booboo/CAREN/Api/GameControl.java
+++ CAREN/src/main/java/com/booboo/CAREN/Api/GameControl.java
@@ -1,19 +1,21 @@
 package com.booboo.CAREN.Api;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.booboo.CAREN.Model.Gameplay;
+import com.booboo.CAREN.Model.Position;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/booboo")
 public class GameControl {
 
+    Gameplay game = Gameplay.getInsGameplay();
 
-    @PostMapping()
-    @RequestMapping("/shop")  //localhost:8080/booboo/shop
-    public String buyAntiA(@RequestBody ShopRequest request){
-        return "buy"+request;
+    @CrossOrigin
+    @GetMapping("/pos")
+    public Position getPos(){
+        Position pos = new Position(1,3);
+        return pos;
     }
 
 }
