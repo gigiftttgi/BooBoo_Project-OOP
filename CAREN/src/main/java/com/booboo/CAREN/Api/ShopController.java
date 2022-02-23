7250a253295f1c13885e20e62b3cc724f4f621a9
minmeeen <sirapat.mp@gmail.com>
2022-02-23 14:53:30 +0700

    can click to sent request

--- CAREN/src/main/java/com/booboo/CAREN/Api/ShopController.java
+++ CAREN/src/main/java/com/booboo/CAREN/Api/ShopController.java
@@ -25,17 +25,26 @@ public class ShopController {
 
     @CrossOrigin
     @PostMapping()
-    @RequestMapping("/buy")
-    public String buyAnti(@RequestBody String antibody){
-        if(antibody == "antiA"){
-            shop.buyAntiA();
-        }else if(antibody == "antiB"){
-            shop.buyAntiB();
-        }else if(antibody == "antiC"){
-            shop.buyAntiC();
-        }
+    @RequestMapping("/buyA")
+    public String buyantiA(@RequestBody String antibody){
+        shop.buyAntiA();
+        return "Sent Request Successfully, " + antibody +shop.getMoney();
 
-        return "Sent Request Successfully";
+    }
 
+    @CrossOrigin
+    @PostMapping
+    @RequestMapping("/buyB")
+    public String buyantiB(@RequestBody String anti){
+        shop.buyAntiB();
+        return "Sent Request Successfully, " + anti + shop.getMoney();
+    }
+
+    @CrossOrigin
+    @PostMapping
+    @RequestMapping("/buyC")
+    public String buyantiC(@RequestBody String anti){
+        shop.buyAntiC();
+        return "Sent Request Successfully, " + anti +shop.getMoney();
     }
 }
