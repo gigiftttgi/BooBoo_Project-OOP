44e33228235b594c1095031bde706c35238478e4
gigiftttgi <natcha_si@cmu.ac.th>
2022-02-23 18:53:14 +0700

    add position

--- CAREN/src/main/java/com/booboo/CAREN/Model/Antibody_A.java
+++ CAREN/src/main/java/com/booboo/CAREN/Model/Antibody_A.java
@@ -2,11 +2,12 @@ package com.booboo.CAREN.Model;
 
 public class Antibody_A  extends Antibody {
 
-    Antibody_A(){
+    Antibody_A(int posx, int posy){
         this.type = "antibody";
         this.atk = 15;
         this.hp = 100;
         this.moveCost = 10;
+        this.pos = new Position(posx, posy);
         this.filename = "CAREN/src/main/java/com/booboo/CAREN/Model/Genetic/Genetic_A.txt";
         
     }
--- CAREN/src/main/java/com/booboo/CAREN/Model/Antibody_B.java
+++ CAREN/src/main/java/com/booboo/CAREN/Model/Antibody_B.java
@@ -2,11 +2,12 @@ package com.booboo.CAREN.Model;
 
 public class Antibody_B extends Antibody {
 
-    Antibody_B(){
+    Antibody_B(int posx, int posy){
         this.type = "antibody";
         this.atk = 10;
         this.hp = 120;
         this.moveCost = 15;
+        this.pos = new Position(posx, posy);
         this.filename = "CAREN/src/main/java/com/booboo/CAREN/Model/Genetic/Genetic_B.txt";
     }
     
