59c965842a735f349e3ac9e3bc8c88cd23c9eb3b
gigiftttgi <natcha_si@cmu.ac.th>
2022-03-14 19:32:58 +0700

    field front end

--- frontend/src/component/Field.js
+++ frontend/src/component/Field.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import APIService from './APIService';
+import Cell from './Cell';
 
 const Field  = () => {
 
 
    return(
        <div className="Field">
+            {/* <div className = "Cell">
+           
+
+           </div> */}
+           {/* <script>
+               var max = 10;
+           document.write("<table className = "Table">");
+
+                document.write("<tr>");
+
+                for(var j = 1; j < max; j++){
+                    document.write("<tr className = 'Cell' > </td>");
+                }       
+
+                document.write("</tr>"); 
+            
+           document.write("</table>");
+
+           </script> */}
+            
+            <table className = "Table">
+               
+                    {Array.from({length:17}, _ =>
+                        <tr>{Array.from({length:25}, _ => <td className = "Cell" ></td> )}</tr>
+                    )}
+                
+            </table>
        </div>
+       
 
    );
 
@@ -14,6 +43,7 @@ const Field  = () => {
 
 
 }
+
 
 
 export default Field;
