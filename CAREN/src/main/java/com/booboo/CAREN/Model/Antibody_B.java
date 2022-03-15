99410d38db4c53f4baaeaf92e0d956ef3048c772
gigiftttgi <natcha_si@cmu.ac.th>
2022-03-16 00:42:25 +0700

    edit atk and hp

--- CAREN/src/main/java/com/booboo/CAREN/Model/Antibody_B.java
+++ CAREN/src/main/java/com/booboo/CAREN/Model/Antibody_B.java
@@ -4,8 +4,8 @@ public class Antibody_B extends Antibody {
 
     Antibody_B(int posx, int posy){
         this.type = "antibody";
-        this.atk = 10;
-        this.hp = 120;
+        this.atk = 20;
+        this.hp = 80;
         this.moveCost = 15;
         this.pos = new Position(posx, posy);
         this.filename = "CAREN/src/main/java/com/booboo/CAREN/Model/Genetic/Genetic_B.txt";
