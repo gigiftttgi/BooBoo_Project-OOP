e847a65076f387fe0a1c9c87ae09ff3530f5311e
minmeeen <sirapat.mp@gmail.com>
2022-03-15 22:25:31 +0700

    can buy and sel pos antibody

--- CAREN/src/main/java/com/booboo/CAREN/Api/Position.java
+++ CAREN/src/main/java/com/booboo/CAREN/Api/Position.java
@@ -1,14 +1,24 @@
 package com.booboo.CAREN.Api;
 
 public class Position {
-    private int posx;
-    private int posy;
+    private int p;
 
-    public int getPosx() {
-        return posx;
+    public int getP(){
+        return p;
     }
 
-    public int getPosy(){
-        return posy;
-    }
+
+
+//    public int getPosx() {
+//        int x = p)- y) / 25) + 1;
+//        return posx;
+//    }
+//
+//    public int getPosy(){
+//        int y = p%25;
+//        if(y==0){
+//            y=25;
+//        }
+//        return y;
+//    }
 }
--- frontend/src/component/Cell.js
+++ frontend/src/component/Cell.js
@@ -5,25 +5,11 @@ import axios from 'axios';
 const Cell = ({ id, SentAntiF }) => {
 
   const pos = id;
-  var y = pos.id % 25;
-  if (y == 0) y = 25;
-  var x = ((pos.id - y) / 25) + 1;
+  // var y = pos.id % 25;
+  // if (y == 0) y = 25;
+  // var x = ((pos.id - y) / 25) + 1;
 
   const [anti, setAnti] = useState('');
-
-
-  // useEffect(async () => {
-  //   fetchAnti()
-
-  //   const interval = setInterval(() => {
-  //     fetchAnti()
-  //   }, 1000)
-  //   return () => clearInterval(interval)
-
-
-  // }, []);
-
-
 
 
   function sentpos() {
@@ -36,16 +22,23 @@ const Cell = ({ id, SentAntiF }) => {
 
       console.log("It's A", pos);
 
-      axios.post('/shop/buyA', pos)
+      axios.post('/shop/buyA',
+        { "p": pos }
+      )
         .then(response => console.log(response.data));
 
     } else if (SentAntiF === "B") {
-      console.log("It's B",pos);
-      axios.post('/shop/buyB', pos)
+      console.log("It's B", pos
+      );
+      axios.post('/shop/buyB', 
+      { "p": pos }
+      )
         .then(response => console.log(response.data));
     } else if (SentAntiF === "C") {
-      console.log("It's C",pos);
-      axios.post('/shop/buyC', pos)
+      console.log("It's C", pos);
+      axios.post('/shop/buyC', 
+      { "p": pos }
+      )
         .then(response => console.log(response.data));
     }
   }
