f8fe202f4d7cf3451725a89f589bd65aecdfb559
kkob999 <chom.kappa@gmail.com>
2022-03-16 14:36:11 +0700

    edit game page from welcom page

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -7,38 +7,47 @@ import Shop from './component/Shop';
 
 function Game() {
 
-  const [gameover, setGameover] = useState('');
-  //const [Pos,setPos] = useState(null);
-  const [Anti,SetAnti] = useState(null);
-  if (gameover === "Gameover") {
-    alert("GameOver");
-  }
-
-  // const PositionApp = (pos) =>{
-  //   setPos(pos);
-  //   // console.log(pos);
-  // } 
-  const SentAnti = () => {
-    SetAnti(Anti);
-  }
-
-  const Antibody = (anti) => {
-    SetAnti(anti);
-    console.log(anti);
-  }
-
-  return (
-
-    <div className='background'>
-      <Field SentAnti = {Anti}>
+    const [gameover, setGameover] = useState('');
+    const [Anti, SetAnti] = useState(null);
+    //const [position, setPosition] = useState(null);
+    if (gameover === "Gameover") {
+      alert("GameOver");
+    }
+  
+    const SentPos = (pos) => {
+    
+      if (Anti === "A") {
+        console.log("It's A", pos);
+        axios.post('/shop/buyA', { "p": pos })
+          .then(response => console.log(response.data));
+      } else if (Anti === "B") {
+        console.log("It's B", pos);
+        axios.post('/shop/buyB', { "p": pos })
+          .then(response => console.log(response.data));
+      } else if (Anti === "C") {
+        console.log("It's C", pos);
+        axios.post('/shop/buyC', { "p": pos })
+          .then(response => console.log(response.data));
+      }
+  
+    }
+  
+    const Antibody = (anti) => {
+      SetAnti(anti);
+      console.log(anti);
+    }
+  
+    return (
+  
+      <div>
+        <Field SentPos = {SentPos}>
         </Field>
-      <div className='Container'>
-        <Menu></Menu>
-        <Shop Antibody = {Antibody} ></Shop>
+        <div className = 'Container'>
+          <Menu></Menu>
+          <Shop Antibody = {Antibody} ></Shop>
+        </div>
+  
       </div>
-
-    </div>
-
   );
 }
 
