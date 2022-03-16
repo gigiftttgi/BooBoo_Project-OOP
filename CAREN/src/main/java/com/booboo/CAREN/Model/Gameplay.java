21b3f017dbee28a0064d97c75ed58f12bd591706
kkob999 <chom.kappa@gmail.com>
2022-03-17 01:41:38 +0700

    test speedUp and speedDown btn

--- CAREN/src/main/java/com/booboo/CAREN/Model/Gameplay.java
+++ CAREN/src/main/java/com/booboo/CAREN/Model/Gameplay.java
@@ -1,6 +1,6 @@
 package com.booboo.CAREN.Model;
 
-import com.booboo.CAREN.Controller.Button;
+//import com.booboo.CAREN.Controller.Button;
 import com.booboo.CAREN.Parser.SyntaxError;
 import lombok.SneakyThrows;
 
@@ -14,6 +14,7 @@ import java.util.List;
 public class Gameplay {
 
   public void pauseGame() throws InterruptedException{
+    System.out.println("pauseGame");
     synchronized (this){
       System.out.println("Going to wait");
       wait();
@@ -21,18 +22,44 @@ public class Gameplay {
     }
   }
 
-  public void resumeGame(boolean pauseState) throws InterruptedException{
-
+  public void resumeGame(boolean state) throws InterruptedException{
+    System.out.println("resumeGame");
     System.out.println("Pause");
-    Thread.sleep(5000);
+    pNum = 0;
+//    Thread.sleep(5000);
     synchronized (this){
-      if (pauseState==true){
+
+      if (state==true){
         notify();
       }
     }
   }
 
+  boolean btn_speedUp = false;
+  boolean btn_speedDown = false;
+  boolean btn_pause = false;
+  boolean rsm = false;
+  int pNum = 0;
+
+
+
+  public void setPauseState(boolean state){
+    System.out.println("setPauseState");
+    btn_pause = state;
+    pNum = 1;
+  }
+
+  public void setSpeedUpState(boolean state){
+    btn_speedUp = state;
+  }
+
+  public void setSpeedDownState(boolean state){
+    btn_speedDown = state;
+  }
+
+
   public Thread createThread_pause(){
+    System.out.println("createPauseT");
     Thread t_pause = new Thread(new Runnable() {
       @SneakyThrows
       @Override
@@ -43,18 +70,22 @@ public class Gameplay {
     return t_pause;
   }
 
+//  Button pBtn = new Button("pause");
   public Thread createThread_resume(){
+    System.out.println("createResumeT");
     Thread t_resume = new Thread(new Runnable() {
       @SneakyThrows
       @Override
       public void run() {
-        resumeGame(true);
+//        System.out.println("run p" + pBtn.getPauseState());
+        resumeGame(rsm);
       }
     });
     return t_resume;
   }
 
   public void runThread() throws InterruptedException {
+    System.out.println("runT");
     Thread tr1 = createThread_pause();
     Thread tr2 = createThread_resume();
     tr1.start();
@@ -65,9 +96,9 @@ public class Gameplay {
 
   }
 
+
   public void startGame() throws InterruptedException{
     boolean endState = false;
-    boolean isPause = false;
     Field field = Field.getInstance();
     Characterfactory fac = new Characterfactory();
     //สมมุติว่าตรงนี้กำหนด10วิ ก่อนที่จะเริ่มเช็คว่าในfield ยังมี antibody or virus อยู่รึเปล่า
@@ -75,34 +106,58 @@ public class Gameplay {
     field.addVirus(x);
     Antibody_A a = new Antibody_A(5, 4);
     field.addAntibody(a);
+    Antibody_B b = new Antibody_B(5, 6);
+    field.addAntibody(b);
     List<Virus> listV = field.getListVirus();
     List<Antibody> listA = field.getListAntibody();
-    Button btn_speedUp = new Button("speedUp");
-    Button btn_speedDown = new Button("speedDown");
-    Button btn_pause = new Button("pause");
 
-    //ครั้งแรกเท่านั้น เพราะอยู่นอกลูป
-    btn_pause.setPauseState(true);
     while(endState==false){
-      int sp = 2;
+      //create virus every 5 second / normal mode
+      int sp = 5;
       Time time = new Time();
-      if(listA.isEmpty()||listV.isEmpty()) endState = true;
-      if(btn_speedUp.speedUpBtn()){
-        System.out.println("jjjjjj");
-        sp = time.getSpeed();
-      }else if(btn_speedDown.speedDownBtn()){
-        sp = time.getSpeed();
-      }else if(btn_pause.pauseBtn()){
+
+      if(listV.isEmpty()) {
+        endState = true;
+        break;}
+
+      if(btn_speedUp){
+        if (sp==5){
+          System.out.println("speedUp is press, before speed is "+sp);
+          sp = 3;
+          System.out.println("speedUp is press, after speed is "+sp);
+        }else if(sp == 3){
+          sp = 3;
+          System.out.println("speedUp is press, But you cant go further "+sp);
+        }else if(sp == 7){
+          System.out.println("speedUp is press, before speed is "+sp);
+          sp = 5;
+          System.out.println("speedUp is press, after speed is "+sp);
+        }
+      }else if(btn_speedDown){
+        if (sp==5){
+          System.out.println("speedDown is press, before speed is "+sp);
+          sp = 7;
+          System.out.println("speedDown is press, after speed is "+sp);
+        }else if(sp == 7){
+          sp = 7;
+          System.out.println("speedDown is press, But you cant go further "+sp);
+        }else if(sp == 3){
+          System.out.println("speedDown is press, before speed is "+sp);
+          sp = 5;
+          System.out.println("speedDown is press, after speed is "+sp);
+        }
+      }else if(btn_pause && pNum==1){
         runThread();
-//        isPause = true;
-//        System.out.println("Pause status: "+isPause);
       }
 
       System.out.println("now sec is: "+time.getcurrTime());
-      fac.createVirus();
+      System.out.println("now speed is: "+sp);
+      // fac.createVirus();
       try {
-        for(Gamecharacter g : field.getAllChar())
+        for(int i = 0; i < field.getAllChar().size() ; i++){
+          Gamecharacter g = field.getAllChar().get(i);
           g.runGeneticcode();
+        }
       } catch (FileNotFoundException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
@@ -112,11 +167,6 @@ public class Gameplay {
       }
       System.out.println("===============================================");
 
-//      if (isPause==true ) {
-//        runThread();
-//      }else{
-//        Thread.sleep(sp*1000);
-//      }
       Thread.sleep(sp*1000);
 
     }
